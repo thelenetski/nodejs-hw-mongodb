c9afba559c67d4af75575d1e8953f534e130f258
thelenetski <49438367+thelenetski@users.noreply.github.com>
2024-10-20 11:24:35 +0300

    change error name

--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -7,7 +7,6 @@ export const validateBody = (schema) => async (req, res, next) => {
     });
     next();
   } catch (err) {
-    /*Чому по конспекту тут помилка прокидується у некст, а ні через throw?*/
     next(
       createHttpError(400, 'Bad Request', {
         errors: err.details,
