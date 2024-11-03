35bd959dd64006ead7ab9e2383e7e15adc00f284
thelenetski <49438367+thelenetski@users.noreply.github.com>
2024-11-03 19:07:26 +0200

    finish

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import router from './routers/index.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import cookieParser from 'cookie-parser';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -25,6 +26,8 @@ export const startServer = () => {
   );
 
   app.use(router);
+
+  app.use('/api-docs', swaggerDocs());
 
   app.use('*', notFoundHandler);
 
